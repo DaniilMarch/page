c61be76046b6ef915c0bbe549c60df4f09461715
DaniilMarch <asmandian@gmail.com>
2019-08-23 11:54:52 +0300

    Updates

---
+++ precache-manifest.6c044ec2ae05b39183b6c9c0d3b94002.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d397443dc09396ba5e6fade98230e679",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "32f2cb1b60c671601319",
+    "url": "/page/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "32f2cb1b60c671601319",
+    "url": "/page/static/js/2.1f861d4b.chunk.js"
+  },
+  {
+    "revision": "c33873e6b63f74ce2714",
+    "url": "/page/static/js/main.28571996.chunk.js"
+  },
+  {
+    "revision": "0e6203fa64f56f545452",
+    "url": "/page/static/js/runtime~main.2bdf8e49.js"
+  },
+  {
+    "revision": "164e7ee7ccb40bb5f71bf32540da8952",
+    "url": "/page/static/media/bookkeeper.164e7ee7.png"
+  },
+  {
+    "revision": "527df5b2b90bcbe1e1607508d3c217eb",
+    "url": "/page/static/media/bookkeeper.527df5b2.gif"
+  },
+  {
+    "revision": "99e739a14cd416adc8da7612432c6e00",
+    "url": "/page/static/media/face.99e739a1.jpg"
+  },
+  {
+    "revision": "52834a55c9bc8a28ac9f925aeb57f03b",
+    "url": "/page/static/media/hangman.52834a55.png"
+  },
+  {
+    "revision": "70d6679f5120d88df4c6022ca738a4ff",
+    "url": "/page/static/media/hangman.70d6679f.gif"
+  },
+  {
+    "revision": "55b5f9c8a5aefeb1f9764ba9773dbe01",
+    "url": "/page/static/media/picshare.55b5f9c8.png"
+  },
+  {
+    "revision": "496e28db0d40d3d14b43884c6e898ad6",
+    "url": "/page/static/media/picsharegif.496e28db.gif"
+  },
+  {
+    "revision": "5b16405d8a125888409f8883bd6aacf5",
+    "url": "/page/static/media/smalltalk.5b16405d.png"
+  },
+  {
+    "revision": "6beb1bcd39663cdc8129f6b7827ffcba",
+    "url": "/page/static/media/smalltalkgif.6beb1bcd.gif"
+  },
+  {
+    "revision": "6f773d42c58d1dc73160f447b0047301",
+    "url": "/page/static/media/test-taskgif.6f773d42.gif"
+  },
+  {
+    "revision": "dbb33bb5c4e4a817e3d8b2b0e8665e3a",
+    "url": "/page/static/media/test-taskj.dbb33bb5.png"
+  },
+  {
+    "revision": "ab7f47c07e35cea44290dabe7958a628",
+    "url": "/page/static/media/tgbot.ab7f47c0.png"
+  }
+]);
